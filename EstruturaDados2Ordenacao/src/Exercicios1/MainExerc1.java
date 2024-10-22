214ec38dfeff1ffa0efa2a5432d42988c679875c
Wicolly Pedro Alcantara <130376559+TSWill03@users.noreply.github.com>
2024-10-22 09:32:56 -0300

    Update MainExerc1.java

--- EstruturaDados2Ordenacao/src/Exercicios1/MainExerc1.java
+++ EstruturaDados2Ordenacao/src/Exercicios1/MainExerc1.java
@@ -1,10 +1,10 @@
 package Exercicios1;
 
-
 import java.util.Arrays;
 
 public class MainExerc1 {
     public static void main(String[] args) {
+        long[] timesNano = new long[6];
         int[] vetorMain = {3, 4, 9, 2, 5, 8, 1};
         Exiba show = new Exiba();
 
@@ -15,7 +15,6 @@ public class MainExerc1 {
         InsertionSortExerc1 insertionSort = new InsertionSortExerc1();
         QuickSortExerc1 quickSort = new QuickSortExerc1();
 
-
         int[] bubbleSorted = Arrays.copyOf(vetorMain, vetorMain.length);
         int[] insertionSorted = Arrays.copyOf(vetorMain, vetorMain.length);
         int[] selectionSorted = Arrays.copyOf(vetorMain, vetorMain.length);
@@ -25,21 +24,54 @@ public class MainExerc1 {
 
 
         System.out.println("BubbleSort");
+        long startTime = System.nanoTime();
         show.show(bubbleSort.bubbleSort(bubbleSorted));
+        long endTime = System.nanoTime();
+        timesNano[0] = endTime - startTime;
 
         System.out.println("InsertionSort");
+        startTime = System.nanoTime();
         show.show(insertionSort.insertionSort(insertionSorted));
+        endTime = System.nanoTime();
+        timesNano[1] = endTime - startTime;
 
         System.out.println("SelectionSort");
+        startTime = System.nanoTime();
         show.show(selectionSort.selectionSort(selectionSorted));
+        endTime = System.nanoTime();
+        timesNano[2] = endTime - startTime;
 
         System.out.println("ShellSort");
+        startTime = System.nanoTime();
         show.show(shellSort.shellSort(shellSorted));
+        endTime = System.nanoTime();
+        timesNano[3] = endTime - startTime;
 
         System.out.println("MergeSort");
+        startTime = System.nanoTime();
         show.show(mergeSort.mergeSort(mergeSorted));
+        endTime = System.nanoTime();
+        timesNano[4] = endTime - startTime;
+
 
         System.out.println("QuickSort");
+        startTime = System.nanoTime();
         show.show(quickSort.quickSort(quickSorted, 0, quickSorted.length - 1));
+        endTime = System.nanoTime();
+        timesNano[5] = endTime - startTime;
+
+
+
+        System.out.println("Tempo de execução (em nanosegundos, microssegundos e milissegundos) para cada algoritmo:");
+        System.out.println("Execução  | BubbleSort | InsertionSort | SelectionSort | ShellSort | MergeSort | QuickSort");
+
+        System.out.printf("Nanoseg   | %10d | %13d | %13d | %9d | %9d | %9d\n",
+                timesNano[0], timesNano[1], timesNano[2], timesNano[3], timesNano[4], timesNano[5]);
+
+        System.out.printf("Microseg  | %10d | %13d | %13d | %9d | %9d | %9d\n",
+                timesNano[0]/1000, timesNano[1]/1000, timesNano[2]/1000, timesNano[3]/1000, timesNano[4]/1000, timesNano[5]/1000);
+
+        System.out.printf("Miliseg   | %10d | %13d | %13d | %9d | %9d | %9d\n",
+                timesNano[0]/1000000, timesNano[1]/1000000, timesNano[2]/1000000, timesNano[3]/1000000, timesNano[4]/1000000, timesNano[5]/1000000);
     }
 }
