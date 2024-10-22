c36c0fb7f597711242fac2956ce871dcafdefb29
Wicolly Pedro Alcantara <130376559+TSWill03@users.noreply.github.com>
2024-10-22 10:53:49 -0300

    Update MainExerc5.java

--- EstruturaDados2Ordenacao/src/Exercicios2_4_5/MainExerc5.java
+++ EstruturaDados2Ordenacao/src/Exercicios2_4_5/MainExerc5.java
@@ -5,8 +5,7 @@ import java.util.Random;
 
 public class MainExerc5 {
     public static void main(String[] args) {
-        final int tamanhoArray = 100000;
-        final int ValorMaximo = 100000;
+        final int ValorMaximo = 100_000_000;
         final int numExecucoes = 10;
 
         Random random = new Random();
@@ -14,39 +13,51 @@ public class MainExerc5 {
         DualPivotQuickSort dualPivotQuickSort = new DualPivotQuickSort();
         QuickSort quickSort = new QuickSort();
 
-        long[][] times = new long[numExecucoes][2];
+        long[][] timesNano = new long[numExecucoes][2];
 
         for (int i = 0; i < numExecucoes; i++) {
 
-            int[] ArrayOriginal = new int[tamanhoArray];
-            for (int j = 0; j < tamanhoArray; j++) {
+            int[] ArrayOriginal = new int[ValorMaximo];
+            for (int j = 0; j < ValorMaximo; j++) {
                 ArrayOriginal[j] = random.nextInt(ValorMaximo);
             }
 
-            int[] DualpivotArray = Arrays.copyOf(ArrayOriginal, tamanhoArray);
-            int[] quickArray = Arrays.copyOf(ArrayOriginal, tamanhoArray);
+            int[] DualpivotArray = Arrays.copyOf(ArrayOriginal, ValorMaximo);
+            int[] quickArray = Arrays.copyOf(ArrayOriginal, ValorMaximo);
 
-            // Medindo o tempo do DualPivotQuickSort
-            long start = System.currentTimeMillis();
+
+            long start = System.nanoTime();
             dualPivotQuickSort.dualPivotQuickSort(DualpivotArray, 0, DualpivotArray.length - 1);
-            times[i][0] = System.currentTimeMillis() - start;
+            timesNano[i][0] = System.nanoTime() - start;
             System.out.println("Rodou QuickSortDualPivot");
 
-            // Medindo o tempo do QuickSort tradicional
-            start = System.currentTimeMillis();
+
+            start = System.nanoTime();
             quickSort.quickSort(quickArray, 0, quickArray.length - 1);
-            times[i][1] = System.currentTimeMillis() - start;
+            timesNano[i][1] = System.nanoTime() - start;
             System.out.println("Rodou QuickSort");
 
             System.out.println("Execução " + (i + 1) + " finalizada\n");
         }
 
-        // Exibindo os tempos de execução para ambos os algoritmos
-        System.out.println("Tempo de execução (em milissegundos) para cada algoritmo e execução:");
-        System.out.println("Execução  | QuickSortDual | QuickSort");
+
+        System.out.println("Tempo de execução para cada algoritmo e execução:");
+        System.out.println("Execução                   | QuickSortDual |    QuickSort");
         for (int i = 0; i < numExecucoes; i++) {
-            System.out.printf("%9d | %13d | %10d \n",
-                    i + 1, times[i][0], times[i][1]);
+
+            System.out.printf("Execução %2d  (Nanoseg):    | %13d | %13d\n",
+                    i + 1, timesNano[i][0], timesNano[i][1]);
+            
+            System.out.printf("Execução %2d  (Microseg):   | %13d | %13d\n",
+                    i + 1, timesNano[i][0] / 1000, timesNano[i][1] / 1000);
+
+            System.out.printf("Execução %2d  (Miliseg):    | %13d | %13d\n",
+                    i + 1, timesNano[i][0] / 1000000, timesNano[i][1] / 1000000);
+            
+            System.out.printf("Execução %2d  (Seg):        | %13d | %13d\n",
+                    i + 1, timesNano[i][0] / 1000000000, timesNano[i][1] / 1000000000);
+            
+            System.out.println();
         }
     }
 }
